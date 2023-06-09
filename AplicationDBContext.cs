2ff4937c9f5e1a78d3a2f49e7a0497896684e105
Mateo Tobar <107194078+MateoTobar19@users.noreply.github.com>
2023-06-09 07:47:34 -0500

    Add files via upload

---
+++ AplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TobarHeredia.models;
+
+
+namespace TobarHeredia
+{
+    public class AplicationDBContext : DbContext
+    {
+        public AplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
